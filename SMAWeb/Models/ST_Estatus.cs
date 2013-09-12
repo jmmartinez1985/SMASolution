ff9b4aeaa16872e42563215b7da2b164199935bc
IsrraBanton <isrrael_01@yahoo.com>
2013-09-11 20:46:04 -0500

    faqs

--- SMAWeb/Models/ST_Estatus.cs
+++ SMAWeb/Models/ST_Estatus.cs
@@ -17,17 +17,17 @@ namespace SMAWeb.Models
         public ST_Estatus()
         {
             this.AN_Anuncios = new HashSet<AN_Anuncios>();
+            this.FAQs = new HashSet<FAQs>();
             this.SS_SolicitudServicio = new HashSet<SS_SolicitudServicio>();
             this.UserProfile = new HashSet<UserProfile>();
-            this.FAQs = new HashSet<FAQs>();
         }
     
         public int ST_Id { get; set; }
         public string ST_Descripcion { get; set; }
     
         public virtual ICollection<AN_Anuncios> AN_Anuncios { get; set; }
+        public virtual ICollection<FAQs> FAQs { get; set; }
         public virtual ICollection<SS_SolicitudServicio> SS_SolicitudServicio { get; set; }
         public virtual ICollection<UserProfile> UserProfile { get; set; }
-        public virtual ICollection<FAQs> FAQs { get; set; }
     }
 }
