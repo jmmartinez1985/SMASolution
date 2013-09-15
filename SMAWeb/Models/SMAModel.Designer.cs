aea87e2bcec9a0da7edb0f7055c1b3e25147bc18
Jose Martinez <stn_rules@hotmail.com>
2013-09-15 13:53:15 -0500

    Liberando paginacion.

--- SMAWeb/Models/SMAModel.Designer.cs
+++ SMAWeb/Models/SMAModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'C:\Users\Logic\Documents\GitHub\SMASolution\SMAWeb\Models\SMAModel.edmx'. 
+﻿// Default code generation is disabled for model 'C:\Users\Logic\Documents\Visual Studio 2012\Projects\SMASolution\SMAWeb\Models\SMAModel.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
